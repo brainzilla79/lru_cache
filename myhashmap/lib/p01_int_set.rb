529e0de767d7ef183b0793fedf6950e928694e4b
App Academy Student <student@appacademy.io>
2017-08-11 12:16:16 -0700

    Complete phase 1 of myhashmap

---
+++ myhashmap/lib/p01_int_set.rb
@@ -0,0 +1,114 @@
+class MaxIntSet
+  def initialize(max)
+    @max = max
+    @store = Array.new(max) {false}
+  end
+
+  def insert(num)
+    if num <= @max && num >= 0
+      if include?(num)
+        raise "already there"
+      else
+        @store[num] = true
+      end
+    else
+      raise "Out of bounds"
+    end
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    @store[num%num_buckets] << num
+  end
+
+  def remove(num)
+    @store[num%num_buckets].delete(num)
+  end
+
+  def include?(num)
+    @store[num%num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    unless include?(num)
+      @store[num % num_buckets] << num
+      @count += 1
+      resize! if @count == num_buckets
+    end
+  end
+
+  def remove(num)
+    if @store[num % num_buckets].include?(num)
+      @store[num % num_buckets].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    @store[num % num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    @store += Array.new(num_buckets) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |el|
+        bucket.delete(el)
+        @count -= 1
+        insert(el)
+      end
+    end
+    # p @store
+  end
+
+end
